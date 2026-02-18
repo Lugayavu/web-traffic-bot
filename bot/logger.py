96ed59b3d74ce422feb17bca5953fd6648886fcb
Lugayavu <65393673+Lugayavu@users.noreply.github.com>
2026-02-18 20:16:58 +0300

    Add logger setup function to bot/logger.py

---
+++ bot/logger.py
@@ -0,0 +1,38 @@
+import logging
+import sys
+from datetime import datetime
+
+def setup_logger(name, log_file=None):
+    """
+    Setup logging configuration
+    
+    Args:
+        name: Logger name
+        log_file: Optional log file path
+    
+    Returns:
+        Logger instance
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    
+    # Formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    
+    # File handler (if log_file specified)
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    
+    return logger
